0496df806a02ec2fea003845e0df13de2fae3671
Austin Lambert <atlambert11@gmail.com>
2020-12-01 11:24:35 -0600

    Updated Posts page and added Post component

---
+++ client/src/components/Post/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Post() {
+    return (
+        <div className="col-12">
+            <h5>USERNAME OF POSTER HERE</h5>
+            <hr/>
+            <p>POST DESCRIPTION HERE</p>            
+        </div>
+    )
+}
+
+export default Post
