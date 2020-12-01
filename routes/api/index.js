7b592d1d9e7388c7fcfa4d07ae1dc756a00b2728
Elliott Payne <e.payne000@gmail.com>
2020-12-01 11:18:49 -0600

    worked out the problems with auth, auth is working

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const postRoutes = require("./posts");
+//const postRoutes = require("./posts");
 const authRoutes = require("./auth");
 
 // Book routes
-router.use("/posts", postRoutes);
+//router.use("/posts", postRoutes);
 router.use("/", authRoutes);
 
 module.exports = router;
