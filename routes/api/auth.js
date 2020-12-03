1ef02c4b00875f8468888bcf946423c90a360994
Austin Lambert <atlambert11@gmail.com>
2020-12-02 22:51:52 -0600

    I literally don't know

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -66,6 +66,7 @@ router.get("/logout", function (req, res) {
 // Route for getting some data about our user to be used client side
 router.get("/user_data", function (req, res) {
   if (!req.user) {
+    console.log("Failed")
     // The user is not logged in, send back an empty object
     res.status(401).json({});
   } else {
