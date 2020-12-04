c177137733f766338bb696fd531fdaf93a79db04
codyalmand <codybalmand@gmail.com>
2020-12-04 15:33:42 -0600

    Added style to Posts page

--- client/src/components/Post/index.js
+++ client/src/components/Post/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./style.css"
 
-
 function Post(props) {
     return (
         <div className="col-12 border">
-            <h4>{props.title}</h4>
-            <hr/>
-            <h5>USERNAME OF POSTER HERE</h5>
-            <p>{props.description}</p>            
+            <row id="username">UserName</row>
+            <br></br><hr></hr>
+            <p id="title">{props.title}</p>
+            <hr></hr>
+            <p id="description">{props.description}</p>            
         </div>
     )
 }
